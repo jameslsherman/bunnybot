fd3a3e9991b4ea1f0e1128219d3d64e7f4c04fb7
jameslsherman <jameslsherman@yahoo.com>
2020-01-26 18:09:11 -0600

    added sudo

--- upload_to_slack.py
+++ upload_to_slack.py
@@ -48,7 +48,7 @@ def copy_to_local(username, image):
 
     if not os.path.exists(image_location):
         # bash_command = 'sudo gsutil cp gs://' + config['bucket_name'] + '/' + image_location + ' ' + image_location
-        bash_command = 'gsutil cp gs://' + config[
+        bash_command = 'sudo gsutil cp gs://' + config[
             'bucket_name'] + '/' + image_location + ' ' + image_location
         print(u'bash_command: {}'.format(bash_command))
 
