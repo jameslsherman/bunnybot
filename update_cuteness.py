9bd4b73dc673fb820601d6ba700896b566cc73f2
jameslsherman <jameslsherman@yahoo.com>
2020-02-02 13:02:57 -0600

    Update update_cuteness.py

--- update_cuteness.py
+++ update_cuteness.py
@@ -3,12 +3,21 @@ import datetime
 from google.cloud import firestore
 import google.cloud.exceptions
 
+
 #-----------------------------------------------------------------------
 def main():
 
+    update_cuteness()
+    update_posted_dttm()
+
+
+#-----------------------------------------------------------------------
+def update_cuteness():
+
     db = firestore.Client()
 
-    docs = db.collection(u'images').where(u'rabbit_cuteness', u'==', 0).stream()
+    docs = db.collection(u'images').where(u'rabbit_cuteness', u'==',
+                                          0).stream()
     for doc in docs:
         print(u'{} => {}'.format(doc.id, doc.to_dict()))
         sum_rabbit_cuteness = 0
@@ -21,13 +30,21 @@ def main():
         doc_ref = db.collection(u'images').document(doc.id)
         doc_ref.set({'rabbit_cuteness': sum_rabbit_cuteness}, merge=True)
 
+
+#-----------------------------------------------------------------------
+def update_posted_dttm():
+
+    db = firestore.Client()
+
     # Posted Dttm
     docs = db.collection(u'images').where(u'posted_dttm', u'>',
-                                          datetime.datetime(2000, 1, 1)).stream()
+                                          datetime.datetime(2000, 1,
+                                                            1)).stream()
     for doc in docs:
         print(u'{} => {}'.format(doc.id, doc.to_dict()))
         doc_ref = db.collection(u'images').document(doc.id)
         doc_ref.update({u'posted_dttm': firestore.DELETE_FIELD})
+
 
 #-----------------------------------------------------------------------
 if __name__ == "__main__":
