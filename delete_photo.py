1b5de5a35e69855b55dfe96cd7b651df031c66d1
jameslsherman <jameslsherman@yahoo.com>
2020-02-02 13:41:46 -0600

    Update delete_photo.py

--- delete_photo.py
+++ delete_photo.py
@@ -7,6 +7,22 @@ import google.cloud.exceptions
 config = yaml.safe_load(open("config.yaml"))
 
 #-----------------------------------------------------------------------
+def get_bunny_image():
+    db = firestore.Client()
+
+    images_ref = db.collection(u'images')
+    query = images_ref.where('rabbit_cuteness','<=',0)
+    results = query.stream()
+
+    for result in results:
+        print(u'{} => {}'.format(result.id, result.to_dict()))
+        print('\n')
+        # delete_photo(config['bucket_name'],'')
+
+    return result.to_dict()['username'], result.id
+
+
+#-----------------------------------------------------------------------
 def delete_photo(bucket_name, blob_name):
     """Deletes a blob from the bucket."""
     # bucket_name = "your-bucket-name"
@@ -20,22 +36,8 @@ def delete_photo(bucket_name, blob_name):
 
     print("Blob {} deleted.".format(blob_name))
 
-#-----------------------------------------------------------------------
-def get_bunny_image():
-    db = firestore.Client()
-
-    images_ref = db.collection(u'images')
-    query = images_ref.where('rabbit_cuteness','<=',0)
-    results = query.stream()
-
-    for result in results:
-        print(u'{} => {}'.format(result.id, result.to_dict()))
-        print('\n')
-
-    return result.to_dict()['username'], result.id
 
 #-----------------------------------------------------------------------
-# delete_photo(config['bucket_name'],'')
 username, image = get_bunny_image()
 print(username)
 print(image)
